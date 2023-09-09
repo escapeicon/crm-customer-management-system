5dc9b064f4ac8c2d98b65984682e3306cc198846
慕思宇 <escapeicon@gmail.com>
2023-09-09 13:23:38 +0800

    调试至可以访问首页

--- crm/src/main/java/com/mycode/crm/settings/web/controller/UserController.java
+++ crm/src/main/java/com/mycode/crm/settings/web/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.mycode.crm.web.controller;
+package com.mycode.crm.settings.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
 
-    @RequestMapping("/settings/qx/user/login.html")
+    @RequestMapping("/settings/qx/user/toLogin.do")
     public String login(){
+        System.out.println("到达此处");
         return "settings/qx/user/login";
     }
 }
--- crm/src/main/webapp/WEB-INF/pages/index.jsp
+++ crm/src/main/webapp/WEB-INF/pages/index.jsp
@@ -4,8 +4,11 @@
 <meta charset="UTF-8">
 </head>
 <body>
+	<%
+		System.out.println(1);
+	%>
 	<script type="text/javascript">
-		window.location.href = "settings/qx/user/login.jsp";
+		window.location.href = "/settings/qx/user/toLogin.do";
 	</script>
 </body>
 </html>
